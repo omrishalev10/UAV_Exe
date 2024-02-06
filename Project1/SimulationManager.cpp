fe7eee0e22c14fe298b828f20c0f27bdc6aca62f
Omri Shalev <73776059+omrishalev10@users.noreply.github.com>
2024-02-06 13:53:04 +0200

    Add files via upload

--- Project1/SimulationManager.cpp
+++ Project1/SimulationManager.cpp
@@ -12,7 +12,8 @@ void SimulationManager::init(const CommandsSet& cmds, const ParsedData& params) 
     this->simulationParams = params;
 
     /* Initialize UAVs based on ParsedData */
-    for (int i = 0; i < params.m_number; ++i) {
+    for (int i = 0; i < params.m_number; ++i) 
+    {
         UavParams uavParams{ i + 1, params.m_radius, params.m_x0, params.m_y0, params.m_z0, params.m_speed0, params.m_azimuth };
         CUav uav;
         uav.initialize(uavParams);
@@ -22,31 +23,24 @@ void SimulationManager::init(const CommandsSet& cmds, const ParsedData& params) 
         ostringstream fileName;
         fileName << "UAV" << i + 1 << ".txt";
         auto handler = make_unique<CFileHandler>(); // Create a unique_ptr to CFileHandler
-        if (!handler->initHandler(fileName.str())) {
+        if (!handler->initHandler(fileName.str())) 
+        {
             cerr << "Failed to initialize file handler for UAV" << (i + 1) << endl;
         }
         fileHandlers.emplace(i + 1, move(handler)); // Store the unique_ptr in the map
      }
 }
 
-void SimulationManager::runSimulation() {
+void SimulationManager::runSimulation() 
+{
     const double Dt = simulationParams.m_Dt; // Time step interval
     auto nextCmdIt = commands.commands.begin(); // Iterator to the first command
 
     // Loop until the simulation time reaches the limit
-    while (currentTime <= simulationParams.m_timeLim) {
-        // Execute commands scheduled for the current time or earlier
-        while (nextCmdIt != commands.commands.end() && nextCmdIt->time <= currentTime) {
-            for (auto& uav : uavs) {
-                if (uav.getUavNumber() == nextCmdIt->uavNumber) {
-                    uav.executeCommand(*nextCmdIt);
-                }
-            }
-            ++nextCmdIt; // Move to the next command
-        }
-
+    while (currentTime <= simulationParams.m_timeLim) 
+    {
         // Update each UAV's state based on Dt
-        for (auto& uav : uavs) 
+        for (auto& uav : uavs)
         {
             if (currentTime == 0.0)
             {
@@ -57,26 +51,29 @@ void SimulationManager::runSimulation() {
                 uav.update(Dt);
             }
         }
-
-        for (auto& uav : uavs) {
+        for (auto& uav : uavs)
+        {
             ostringstream data;
-            /* This is the initialize states of the uav */
-            /*if (currentTime == 0) {
-                data << fixed << setprecision(2)
-                    << "Time: " << currentTime << ", UAV: " << uav.getUavNumber()
-                    << ", X: " << simulationParams.m_x0 << ", Y: " << simulationParams.m_y0
-                    << ", Azimuth: " << simulationParams.m_azimuth << endl;
-            }*/
-            /* else {*/
             data << fixed << setprecision(2)
-                << "Time: " << currentTime << ", UAV: " << uav.getUavNumber()
-                << ", X: " << uav.getX() << ", Y: " << uav.getY()
-                << ", Azimuth: " << uav.getAzimuth() << endl;
-            //}
+            << "Time: " << currentTime << ", UAV: " << uav.getUavNumber()
+            << ", X: " << uav.getX() << ", Y: " << uav.getY()
+            << ", Azimuth: " << uav.getAzimuth() << endl;
+           
             // Write to the corresponding file
             fileHandlers[uav.getUavNumber()]->writeHandler(data.str());
         }
-
+        // Execute commands scheduled for the current time or earlier
+        while (nextCmdIt != commands.commands.end() && nextCmdIt->time <= currentTime) 
+        {
+            for (auto& uav : uavs) 
+            {
+                if (uav.getUavNumber() == nextCmdIt->uavNumber) 
+                {
+                    uav.executeCommand(*nextCmdIt);
+                }
+            }
+            ++nextCmdIt; // Move to the next command
+        }
         currentTime += Dt;
     }
 }
